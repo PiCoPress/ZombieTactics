0aacb670a7c80e7ed52361cc470d7b96fbcfc38f
PICOPress <choys041011@gmail.com>
2025-03-29 22:54:53 +0900

    Hotfix

--- src/main/java/n643064/zombie_tactics/mixin/ZombieMixin.java
+++ src/main/java/n643064/zombie_tactics/mixin/ZombieMixin.java
@@ -61,12 +61,13 @@ public abstract class ZombieMixin extends Monster implements SmartBrainOwner<Zom
         if (entity != null) {
             AABB aabb1 = entity.getBoundingBox();
             AABB aabb2 = this.getBoundingBox();
-            aabb = new AABB(Math.min(aabb2.minX, aabb1.minX), aabb2.minY, Math.min(aabb2.minZ, aabb1.minZ),
-                    Math.max(aabb2.maxX, aabb1.maxX), aabb2.maxY, Math.max(aabb2.maxZ, aabb1.maxZ));
+            aabb = new AABB(Math.min(aabb2.minX, aabb1.minX), Math.min(aabb2.minY, aabb1.maxY), Math.min(aabb2.minZ, aabb1.minZ),
+                    Math.max(aabb2.maxX, aabb1.maxX), Math.max(aabb2.maxY, aabb1.maxY), Math.max(aabb2.maxZ, aabb1.maxZ));
         } else {
             aabb = this.getBoundingBox();
         }
-        return aabb.inflate(Config.attackRange, 0., Config.attackRange);
+        // I think it is a range
+        return aabb.inflate(Config.attackRange, Config.attackRange, Config.attackRange);
     }
 
     protected ZombieMixin(EntityType<? extends Zombie> entityType, Level level) {
@@ -77,6 +78,8 @@ public abstract class ZombieMixin extends Monster implements SmartBrainOwner<Zom
     @Inject(method = "doHurtTarget", at = @At("HEAD"))
     public void doHurtTargetHead(Entity entity, CallbackInfoReturnable<Boolean> cir) {
         MiningData dat = this.getData(Main.ZOMBIE_MINING);
+        // Wither is zombie's friend
+        //System.out.println(entity.getType().is(EntityTypeTags.WITHER_FRIENDS));
         if(dat.doMining) {
             dat.doMining = false;
             System.out.println("I caught you!!");
