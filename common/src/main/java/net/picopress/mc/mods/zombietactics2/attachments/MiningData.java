ac88da5e25115fdc2a935f9efff8698648516e5b
PICOPress <choys041011@gmail.com>
2025-05-09 22:10:29 +0900

    independent of fork

---
+++ common/src/main/java/net/picopress/mc/mods/zombietactics2/attachments/MiningData.java
@@ -0,0 +1,22 @@
+package net.picopress.mc.mods.zombietactics2.attachments;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.phys.Vec3;
+
+
+/*
+    This class contains a mining block's position
+    and a condition of mine for each of zombie
+ */
+public class MiningData {
+    public boolean doMining;
+    public BlockPos bp;
+    public Vec3 bp_vec3; // bp_vec3 = bp.getCenter()
+
+    public MiningData() {
+        doMining = false;
+        // `bp` is Nullable but, if `doMining` is true, `bp` must not be null
+        bp_vec3 = null;
+        bp = null;
+    }
+}
